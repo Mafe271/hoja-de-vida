42d9c56c56be53bef4febcd549d57d3bb7a0c935
Juan Dev <rexejej@gmail.com>
2021-06-10 10:41:33 -0500

    estado del 80%

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import Todo from './components/Todo'
 import Cuerpo from './components/Cuerpo'
 import Footer from './components/Footer';
 import Title from './components/Title';
+import GlobalStyles from './globalStyles';
 
 
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <Title />
       <Todo />
       <Cuerpo />
       <Footer/>
-     
     </div>
   );
 }
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -8,7 +8,9 @@ function Todo() {
             <div className="ContainerText">
                 <h1>Hola este es el componente de la cabecera</h1>
                 <p>
-
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit sunt illo minima adipisci nulla odit quas, voluptate, quibusdam fugit id neque ab minus veniam. Numquam odio amet neque maxime obcaecati.
+                    Culpa eveniet repellat praesentium corporis debitis aliquid nesciunt ad fugiat, excepturi quibusdam quidem sapiente voluptas cupiditate pariatur labore explicabo reprehenderit non ex minus esse commodi quod. Veritatis nihil iure reprehenderit.
+                    Iusto quaerat, nam sequi delectus fugit fuga corporis. Reprehenderit ratione perferendis exercitationem fuga voluptatum ducimus quo explicabo nemo optio tempora! Soluta animi nemo voluptates, accusamus illo tenetur laboriosam sapiente doloribus?
                 </p>
             </div>
             <div className="ContainerImage">
