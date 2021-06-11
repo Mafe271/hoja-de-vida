1b886227de27938ad1f622d237d4103d5c944b50
Juan Dev <rexejej@gmail.com>
2021-06-11 08:47:50 -0500

    cambio en el titulo

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,7 +4,7 @@ import  '../utils/css/Title.estilos.css'
 function Title() {
     return (
         <div className="Container_title">
-            <h1 id="principalTitle">ARTE ABSTRACTO</h1>
+            <h1 id="principalTitle"> IMAGENES DE ARTE ABSTRACTO</h1>
         </div>
     )
 }
