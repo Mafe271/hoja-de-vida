3cdcbb313737279de843a204819cd313e31cb9dc
Juan Dev <rexejej@gmail.com>
2021-06-11 08:22:33 -0500

    estado del 90%

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,4 +1,5 @@
 import  {createGlobalStyle} from 'styled-components'
+import PrimaryFont from './utils/fonts/Hind-Light.woff'
 
 
 const GlobalStyles = createGlobalStyle`
@@ -10,6 +11,12 @@ const GlobalStyles = createGlobalStyle`
     font-family: Impact;
     font-size:20px;
 }
+.ContainerText p{
+    font-family: 'Hind';
+    font-size: 30px;
+    margin: 0 20px;
+}
+
 .ContainerImage{
     display: flex;
     width: 50%;
@@ -19,6 +26,14 @@ const GlobalStyles = createGlobalStyle`
 .ContainerImage img{
     width: 100%;
     height: auto;
+}
+
+/* fuentes*/
+
+@font-face {
+    font-family: 'Hind';
+    src: local('Hind'), url(${PrimaryFont}) format('woff');
+    font-style: normal;
 }
 `;
 
